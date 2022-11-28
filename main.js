0ca6dd220e0bb35e26c1871976bf4b2a7f15e55f
Sciencewolf <aron17marton@gmail.com>
2022-11-28 22:18:20 +0100

    Removed unnecessary lines

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 const setPasswordVisibility = () => {
     const inp = document.getElementById('input-field2')
-    const but = document.getElementById('btn-vis')
-    if(inp.type === "password") {
-        inp.type = "text"
-        but.innerHTML = "Hide password"
-
-    }
-    else {
-        inp.type = "password"
-        but.innerHTML = "Show password"
-    }
+    if(inp.type === "password") inp.type = "text"
+    else inp.type = "password"
 }
